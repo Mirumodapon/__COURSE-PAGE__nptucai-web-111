b0481cafc73c6f63fdbe59e76e94af148c203fd4
Mirumo <mysteryperson520@gmail.com>
2022-09-14 02:08:32 +0800

    update announcement

--- src/Markdown.jsx
+++ src/Markdown.jsx
@@ -9,7 +9,8 @@ export default function Markdown({ children }) {
     <ReactMarkdown
       components={{
         a: ({ node, ...props }) => <Link {...props} />,
-        p: ({ node, ...props }) => <Typography varient="body2" color="text.secondary" {...props} />
+        p: ({ node, ...props }) => <Typography varient="body2" color="text.secondary" {...props} />,
+        img: ({node, ...prop}) => <img {...prop} width="100%"/>
       }}
       remarkPlugins={[remarkGfm]}
       rehypePlugins={[rehypeRaw]}
