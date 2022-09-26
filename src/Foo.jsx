42eab5a99cce65000176ca51de4ea7a72f1ab406
Mirumo <mysteryperson520@gmail.com>
2022-09-26 09:48:25 +0800

    fix build bug

--- src/Foo.jsx
+++ src/Foo.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 function Foo () {
   return <section style={{width: '100%', display: 'flex', justifyContent: 'center'}}>
     <iframe
+      title="feedback form"
       src="https://docs.google.com/forms/d/e/1FAIpQLSfcaoX-ZShhsbJDXGKS0jX2AiX1km2BRtkXJERIOBNQWuHxNw/viewform?embedded=true"
       frameborder="0" marginheight="0" marginwidth="0" height="600" width="650" style={{height: '1000px', margin: 'auto'}}
       >載入中…
